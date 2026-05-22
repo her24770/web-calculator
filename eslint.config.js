4e5cabe7fb3417eff609a468a0aa675fb0ae7192
her24770 <her24770@uvg.edu.gt>
2026-05-21 21:13:54 -0600

    etructura inicial y limpia con dependencias necesarias declaradas en el readme.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -5,28 +8,25 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
-    rules: {
-      semi: ['error', 'never'],
-      'max-len': ['error', { code: 120 }],
-      quotes: ['error', 'single'],
-      indent: ['error', 2],
-      'comma-dangle': ['error', 'always-multiline'],
-      'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
-      'no-trailing-spaces': 'error',
-      'eol-last': ['error', 'always'],
-    },
+export default defineConfig([globalIgnores(['dist']), {
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    globals: globals.browser,
   },
-])
+  rules: {
+    semi: ['error', 'never'],
+    'max-len': ['error', { code: 120 }],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+    'no-trailing-spaces': 'error',
+    'eol-last': ['error', 'always'],
+  },
+}, ...storybook.configs["flat/recommended"]])
